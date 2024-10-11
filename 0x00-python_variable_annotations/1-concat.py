96f88041f256628d851f20b243a835a9b62e4cca
wikki2000 <wisdomokposin@gmail.com>
2024-10-11 10:09:17 +0100

    Add: 2-floor.py and 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+1-concat.py
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concantenate two string. """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+2-floor.py
+"""
+from math import floor as flr
+
+def floor(n: float) -> int:
+    """ Return floor of a float. """
+    return flr(n)
