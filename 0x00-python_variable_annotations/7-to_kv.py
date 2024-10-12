69690cb188f81a05d707e8d7f5e516b3465b9936
wikki2000 <wisdomokposin@gmail.com>
2024-10-12 13:58:45 +0100

    Add: 7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+7-to_kv.py
+'''
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Return a tuple containing a string and a float'''
+    return k, v * v
