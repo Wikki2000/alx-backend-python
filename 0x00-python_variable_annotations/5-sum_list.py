83c3f01de3ce84661bd532e2dee08d533c2fa520
wikki2000 <wisdomokposin@gmail.com>
2024-10-12 13:40:29 +0100

    Add: 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python3
+'''
+5-sum_list.py
+'''
+from functools import reduce
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    ''' returns sum of all double value of list '''
+    return reduce(lambda a, b: a + b, input_list)
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,4 +7,4 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''Return the sum of lists of integer and float.'''
-    return sum(input_list)
+    return sum(mxd_lst)
